e51b57badb83be56904a80f5e78313edb63b938d
SREEJITH-C <sreejith.c@momscode.in>
2024-05-17 11:32:39 +0530

    Repair order create job wor

--- watch_repair/watch_repair/doctype/repair_order/repair_order.py
+++ watch_repair/watch_repair/doctype/repair_order/repair_order.py
@@ -1,8 +1,39 @@
 # Copyright (c) 2024, sammish and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class RepairOrder(Document):
-	pass
+	
+
+	@frappe.whitelist()
+	def on_submit(self):
+		
+		watch =frappe.get_doc('Watch Service Settings')
+		if watch.auto_create_job_work:
+			for item in self.get('repair_order_item'):
+
+				mr = frappe.new_doc("Job Work")
+				mr.repair_order = self.name
+				mr.company = self.company
+				mr.customer = self.customer
+				mr.customer_name = self.customer_name
+				mr.warehouse = self.warehouse
+
+				mr.service_item = item.item
+				mr.service_item_name = item.item_name
+				mr.qty = item.qty
+				mr.serial_no = item.serial_no
+				mr.model_no = item.model_no
+
+			
+			
+			mr.insert()
+			mr.save()
+
+			self.reload()
+			frappe.msgprint("Job Work created successfully: {}".format(mr.name))
+
+			return mr.name
+ 
